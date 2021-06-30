43f2add29bff93ddebdf858881eb192ea48c0c5a
Claudio Ferreira <claudioferreira.dev@gmail.com>
2021-06-30 19:13:40 -0300

    almost finished navbar

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 import data from './data.js'
 
-function generatePlanets(dbTest) {
-  var tag = document.getElementById('header__main-nav--planet')
 
-  dbTest.forEach((planet) => {
-    tag.innerHTML += planet.name
-  })
-}
 
-generatePlanets(data);
+
