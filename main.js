f86695a95241a479fff0c745c5e371975328e73f
Claudio Ferreira <claudioferreira.dev@gmail.com>
2021-07-01 18:18:42 -0300

    starting main section

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import data from './data.js'
 
-const hamburguerIcon = document.getElementById("header__main-nav--div-img");
+const hamburguerIcon = document.getElementById("header__main-nav--div-anchor");
 const navBarLinks = document.getElementById("header__main-nav--div");
 
 hamburguerIcon.addEventListener("click", function () {
   hamburguerIcon.classList.toggle('change-anchor-color');
-  navBarLinks.classList.toggle("show-div")
+  navBarLinks.classList.toggle("show-div");
+  document.body.classList.toggle("disable-background");
 });
 
 
