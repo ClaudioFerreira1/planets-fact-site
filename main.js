ecd3ee7f4150c389328acb8ea77e52ef93791a23
Claudio Ferreira <claudioferreira.dev@gmail.com>
2021-07-01 12:13:41 -0300

    navbar finished

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import data from './data.js'
 
+const hamburguerIcon = document.getElementById("header__main-nav--div-img");
+const navBarLinks = document.getElementById("header__main-nav--div");
 
+hamburguerIcon.addEventListener("click", function () {
+  hamburguerIcon.classList.toggle('change-anchor-color');
+  navBarLinks.classList.toggle("show-div")
+});
 
 
