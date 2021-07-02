899aa5173601bc3e97ff5f002247c225aa0abcd4
Claudio Ferreira <claudioferreira.dev@gmail.com>
2021-07-02 13:13:56 -0300

    finished main tablet

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import data from './data.js'
 
 const hamburguerIcon = document.getElementById("header__main-nav--div-anchor");
 const navBarLinks = document.getElementById("header__main-nav--div");
+// const surfaceButton = document.getElementById("surface");
+// const secondImage = document.getElementById("upper-section__img-div--second-image");
 
 hamburguerIcon.addEventListener("click", function () {
   hamburguerIcon.classList.toggle('change-anchor-color');
@@ -9,4 +11,7 @@ hamburguerIcon.addEventListener("click", function () {
   document.body.classList.toggle("disable-background");
 });
 
+// surfaceButton.addEventListener("click", function () {
+//   secondImage.style.visibility = "visible";
+// });
 
